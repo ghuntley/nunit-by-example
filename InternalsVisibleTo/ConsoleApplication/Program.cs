6a0e0685198f37d31cda60d63124706f9bd93508
Geoffrey Huntley <ghuntley@ghuntley.com>
2011-12-06 12:40:46 +1100

    explanation and link to MSDN documentation for assembly signing (aka strong name)

--- InternalsVisibleTo/ConsoleApplication/Program.cs
+++ InternalsVisibleTo/ConsoleApplication/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 
+// The assembly name should is slightly different if you want to strong name
+// everything. http://msdn.microsoft.com/en-us/library/wd40t7ad.aspx is
+// worth reading.
 [assembly: InternalsVisibleTo("ConsoleApplication.Tests")]
 namespace ConsoleApplication
 {
